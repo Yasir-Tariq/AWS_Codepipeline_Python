e04b8f6dce4f4dda49d7f42e3b983d9fa061140e
Yasir-Tariq <ynaseem78@gmail.com>
2020-02-24 15:11:53 -0800

    new

--- scripts/install_python3_dependencies.sh
+++ scripts/install_python3_dependencies.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 mkdir /home/ubuntu/sample/
-mkdir /home/ubuntu/deployment
 cd /home/ubuntu/sample/
 pip3 install --user -r requirements.txt
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd /home/ubuntu/sample/
-service python3-pip start
 cd deployment
+service python3-pip start
+# cd deployment
 python3 run_keras_server.py
