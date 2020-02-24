6a139087478ccf379205e1c8a53c5a27c384fba0
Yasir-Tariq <ynaseem78@gmail.com>
2020-02-24 14:38:32 -0800

    new

--- scripts/install_depedencies.sh
+++ scripts/install_depedencies.sh
@@ -1,2 +1,3 @@
+# if ! [ -x "$(command -v python3-pip)" ]; then apt-get install -y python3-pip >&2;   exit 1; fi # install python3-pip if not already installed
 #!/bin/bash
-if ! [ -x "$(command -v python3-pip)" ]; then apt-get install -y python3-pip >&2;   exit 1; fi # install python3-pip if not already installed
+pip3 install --user -r requirements.txt
