9b52e790b61874df71e1876713e1ed37880919a6
Yasir-Tariq <ynaseem78@gmail.com>
2020-02-24 14:46:31 -0800

    new

--- scripts/install_depedencies.sh
+++ scripts/install_depedencies.sh
@@ -1,3 +1,4 @@
 # if ! [ -x "$(command -v python3-pip)" ]; then apt-get install -y python3-pip >&2;   exit 1; fi # install python3-pip if not already installed
 #!/bin/bash
+cd /home/ubuntu/CodePipeline_Assignment
 pip3 install --user -r requirements.txt
